879cdb973616270fae6cfc7ef406cc745ce26c4f
fkamag <fabiokamagian@gmail.com>
2024-06-23 15:58:00 -0300

    feat logar usuário

--- src/desafio.py
+++ src/desafio.py
@@ -5,15 +5,28 @@ class Cliente:
 
 
 class PessoaFisica(Cliente):
-    def __init__(self, cpf, nome, data_nasc, endereco):
+    def __init__(self, cpf, nome, data_nasc, endereco, senha):
         self._cpf = cpf
         self._nome = nome
         self._data_nasc = data_nasc
+        self._senha = senha
         super().__init__(endereco)
 
     @property
     def cpf(self):
         return self._cpf
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def senha(self):
+        return self._senha
+
+    @property
+    def contas(self):
+        return self._contas
 
 
 def buscar_cliente(cpf, clientes):
@@ -33,10 +46,46 @@ def criar_cliente(clientes):
         nome = input('Digite o nome: ')
         data_nasc = input('Digite a data de nascimento [dd-mm-aaaa]: ')
         endereco = input('Digite o endereco: ')
-        cliente = PessoaFisica(cpf, nome, data_nasc, endereco)
+        while True:
+            senha = input('Digite a senha: ')
+            confirma_senha = input('Confirme a senha: ')
+            if senha == confirma_senha:
+                break
+            print('Senha inválida')
+
+        cliente = PessoaFisica(cpf, nome, data_nasc, endereco, senha)
 
         clientes.append(cliente)
         print('Cliente cadastrado com sucesso')
+
+
+def criar_conta():
+    pass
+
+
+def menu_contas(cliente):
+    opcao = input('''
+1 - Criar Conta
+2 - Listar Contas
+3 - Sair
+
+Digite sua opção: ''')
+    if opcao == "1":
+        criar_conta()
+
+
+def listar_contas(cliente):
+    cliente.contas
+
+
+def logar_cliente(clientes):
+    cpf = input('Digite o cpf: ')
+    cliente = buscar_cliente(cpf, clientes)
+    if cliente:
+        senha = input('Digite a senha: ')
+        if cliente.senha == senha:
+            print(f'''\n Bem vindo { cliente.nome }\n''')
+            menu_contas(cliente)
 
 
 def main():
@@ -46,7 +95,7 @@ def main():
         if opcao == "1":
             criar_cliente(clientes)
         elif opcao == "2":
-            print('Logar Usuário')
+            logar_cliente(clientes)
         elif opcao == "3":
             print()
             print('Obrigado por utilizar nossos serviços')
